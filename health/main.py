35ccd221c11138c8c3c467946186059dd041a72c
Ansah Mohammad <85027826+AnsahMohammad@users.noreply.github.com>
2022-01-08 11:54:37 +0530

    Added eye-care extension

---
+++ health/main.py
@@ -0,0 +1,42 @@
+import vscode
+import datetime
+import time
+
+ext = vscode.Extension(name = "testpy", display_name = "Test Py", version = "0.0.1")
+
+@ext.event
+def on_activate():
+    return f"The Extension '{ext.name}' has started"
+
+
+
+@ext.command(keybind="ALT+5")
+def Start_Eye_Care():
+    res = vscode.window.show_quick_pick(['Yes', 'No'])
+    if res == "Yes":
+        time = vscode.window.show_quick_pick(['20 Mins','25 Mins','30 Mins','10 Sec(demo)','None'])
+        if time == "20 Mins":
+            vscode.window.show_info_message("20 mins Eye relax session enabled")
+            call(20*60)
+        elif time == "25 Mins":
+            vscode.window.show_info_message("25 mins Eye relax session enabled")
+            call(25*60)
+        elif time == "30 Mins":
+            vscode.window.show_info_message("30 mins Eye relax session enabled")
+            call(30*60) 
+        elif time == "10 Sec(demo)":
+            vscode.window.show_info_message("10 sec Demo Eye relax session enabled")
+            call(10)
+        else:
+            vscode.window.show_info_message("Eye care Disabled")
+
+
+    elif res == "No":
+        vscode.window.show_info_message('Okay!')
+
+def call(t):
+    time.sleep(t)
+    vscode.window.show_warn_message("its been "+str(t)+ " minutes since you've been working, please take rest by closing your eyes for 20 secs :)")
+    call(t)
+
+vscode.build(ext)
