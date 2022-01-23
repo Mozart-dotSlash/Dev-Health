dd6fd18f4dbb7a70972198bc63cfe445f07a581b
Ansah Mohammad <85027826+AnsahMohammad@users.noreply.github.com>
2022-01-23 12:18:10 +0530

    updated Time notification
    solved the issue where it showed time in seconds in notification.
    Updated it to Minutes format

--- health/main.py
+++ health/main.py
@@ -14,18 +14,23 @@ def on_activate():
 def Start_Eye_Care():
     res = vscode.window.show_quick_pick(['Enable', 'Nope! Had a change of mind :()'],)
     if res == "Enable":
+        global time_set = 0
         time = vscode.window.show_quick_pick(['20 Mins','25 Mins','30 Mins','10 Sec(demo)','None'])
         if time == "20 Mins":
             vscode.window.show_info_message("Confirm 20 mins Eye relax session ", "okay")
+            time_set = "20"
             call(20*60)
         elif time == "25 Mins":
             vscode.window.show_info_message("Confirm 25 mins Eye relax session ", "okay")
+            time_set = "25"
             call(25*60)
         elif time == "30 Mins":
             vscode.window.show_info_message("Confirm 30 mins Eye relax session ", "okay")
+            time_set = "30"
             call(30*60) 
         elif time == "10 Sec(demo)":
             vscode.window.show_info_message("Confirm 10 sec(demo) Eye relax session ", "okay")
+            time_set = "10"
             call(10)
         else:
             vscode.window.show_info_message("Eye care Disabled")
@@ -36,7 +41,7 @@ def Start_Eye_Care():
 
 def call(t):
     time.sleep(t)
-    vscode.window.show_warn_message("its been "+str(t)+ " minutes since you've been working, please take rest by closing your eyes for 20 secs :)","okay")
+    vscode.window.show_warn_message("its been "+time_set+ " minutes since you've been working, please take rest by closing your eyes for 20 secs :)","okay")
     call(t)
 
 vscode.build(ext)
