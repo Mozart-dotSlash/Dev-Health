889c72b7e9d2505e8b7f4d727036540e4369fe8f
Ansah Mohammad <85027826+AnsahMohammad@users.noreply.github.com>
2022-01-26 16:09:46 +0530

    removed demo feature, corrected the typo error.

--- health/main.py
+++ health/main.py
@@ -14,24 +14,19 @@ def on_activate():
 def Start_Eye_Care():
     res = vscode.window.show_quick_pick(['Enable', 'Nope! Had a change of mind :()'],)
     if res == "Enable":
-        global time_set = 0
-        time = vscode.window.show_quick_pick(['20 Mins','25 Mins','30 Mins','10 Sec(demo)','None'])
+        time = vscode.window.show_quick_pick(['20 Mins','25 Mins','30 Mins','None'])
         if time == "20 Mins":
             vscode.window.show_info_message("Confirm 20 mins Eye relax session ", "okay")
-            time_set = "20"
+           
             call(20*60)
         elif time == "25 Mins":
             vscode.window.show_info_message("Confirm 25 mins Eye relax session ", "okay")
-            time_set = "25"
+            
             call(25*60)
         elif time == "30 Mins":
             vscode.window.show_info_message("Confirm 30 mins Eye relax session ", "okay")
-            time_set = "30"
+            
             call(30*60) 
-        elif time == "10 Sec(demo)":
-            vscode.window.show_info_message("Confirm 10 sec(demo) Eye relax session ", "okay")
-            time_set = "10"
-            call(10)
         else:
             vscode.window.show_info_message("Eye care Disabled")
 
@@ -41,8 +36,7 @@ def Start_Eye_Care():
 
 def call(t):
     time.sleep(t)
-    global time_set
-    vscode.window.show_warn_message("its been "+time_set+ " minutes since you've been working, please take rest by closing your eyes for 20 secs :)","okay")
+    vscode.window.show_warn_message("its been "+str(round(t/60,2))+ " minutes since you've been working, please take rest by closing your eyes for 20 secs :)","okay")
     call(t)
 
 vscode.build(ext)
